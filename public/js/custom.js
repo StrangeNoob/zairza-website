85a851389bf10e9d25050bc1f920fe3254291fbf
StrangeNoob <sipun63427452@gmail.com>
2019-10-11 15:53:36 +0530

    Adds Lazy-Loading

--- public/js/custom.js
+++ public/js/custom.js
@@ -1038,3 +1038,33 @@ function detectIE() {
 	// other browser
 	return false;
 }
+document.addEventListener("DOMContentLoaded", function() {
+	var lazyloadImages = document.querySelectorAll("img.lazy");    
+	var lazyloadThrottleTimeout;
+	
+	function lazyload () {
+	  if(lazyloadThrottleTimeout) {
+		clearTimeout(lazyloadThrottleTimeout);
+	  }    
+	  
+	  lazyloadThrottleTimeout = setTimeout(function() {
+		  var scrollTop = window.pageYOffset;
+		  lazyloadImages.forEach(function(img) {
+			  if(img.offsetTop < (window.innerHeight + scrollTop)) {
+				img.src = img.dataset.src;
+				console.log(img.src);
+				img.classList.remove('lazy');
+			  }
+		  });
+		  if(lazyloadImages.length == 0) { 
+			document.removeEventListener("scroll", lazyload);
+			window.removeEventListener("resize", lazyload);
+			window.removeEventListener("orientationChange", lazyload);
+		  }
+	  }, 20);
+	}
+	document.addEventListener("scroll", lazyload);
+	window.addEventListener("resize", lazyload);
+	window.addEventListener("orientationChange", lazyload);
+	window.addEventListener("slider",lazyload);
+  });
